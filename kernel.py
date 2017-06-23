83a7a2d8b65349cdc28b208cbad7a574ce946fa1
Peter Giannakopoulos <petrosgk@users.noreply.github.com>
2017-06-23 11:37:06 +0300

    Add files via upload

--- kernel.py
+++ kernel.py
@@ -2,61 +2,57 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
+
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 import cv2
 from tqdm import tqdm
 
 from sklearn.metrics import fbeta_score
 
-# Params
-input_size = 64
+input_size = 224
 input_channels = 3
 
-epochs = 30
+epochs = 100
 batch_size = 128
-learning_rate = 0.001
-lr_decay = 1e-4
 
-validation_data_size = 5000
-
-# Define model
 model = Sequential()
-model.add(BatchNormalization(input_shape=(input_size, input_size, input_channels)))
-model.add(Conv2D(32, kernel_size=(2, 2), padding='same', activation='relu'))
+
+model.add(Conv2D(16, kernel_size=(2, 2), padding='same', activation='relu',
+                 input_shape=(input_size, input_size, input_channels)))
+model.add(Conv2D(32, kernel_size=(2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, kernel_size=(2, 2), padding='same', activation='relu',))
 model.add(Conv2D(64, kernel_size=(2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Conv2D(64, kernel_size=(2, 2), padding='same', activation='relu'))
 model.add(Conv2D(128, kernel_size=(2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Conv2D(128, kernel_size=(2, 2), padding='same', activation='relu'))
 model.add(Conv2D(256, kernel_size=(2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Conv2D(256, kernel_size=(2, 2), padding='same', activation='relu'))
 model.add(Conv2D(512, kernel_size=(2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
 model.add(Dense(17, activation='sigmoid'))
 
 callbacks = [EarlyStopping(monitor='val_loss',
-                           patience=5,
-                           verbose=0)]
+                           patience=15,
+                           verbose=0),
+             ReduceLROnPlateau(monitor='val_loss',
+                               factor=0.1,
+                               patience=5,
+                               verbose=1)]
 
-opt = Adam(lr=learning_rate, decay=lr_decay)
+opt = Adam(lr=0.001)
 
 model.compile(loss='binary_crossentropy',
               # We NEED binary here, since categorical_crossentropy l1 norms the output before calculating loss.
@@ -65,20 +61,63 @@ model.compile(loss='binary_crossentropy',
 
 df_train_data = pd.read_csv('input/train_v2.csv')
 
+# Use 10% of data for hyper-parameter optimization
+df_train_data = df_train_data[:int(len(df_train_data) * 0.1)]
+
+print(len(df_train_data))
+
 flatten = lambda l: [item for sublist in l for item in sublist]
 labels = list(set(flatten([l.split(' ') for l in df_train_data['tags'].values])))
 
 label_map = {l: i for i, l in enumerate(labels)}
 inv_label_map = {i: l for l, i in label_map.items()}
 
-# Read training samples that will be withheld for validation
+# Calculate mean and stddev of dataset
+mean_sum = np.zeros(3, dtype=np.float32)
+stddev_sum = np.zeros(3, dtype=np.float32)
+
+for f, tags in tqdm(df_train_data.values, miniters=1000):
+    img = cv2.imread('input/train-jpg/{}.jpg'.format(f))
+    mean, stddev = cv2.meanStdDev(src=img)
+    mean_sum[0] += mean[0]
+    mean_sum[1] += mean[1]
+    mean_sum[2] += mean[2]
+    stddev_sum[0] += stddev[0]
+    stddev_sum[1] += stddev[1]
+    stddev_sum[2] += stddev[2]
+
+mean = mean_sum / len(df_train_data)
+stddev = stddev_sum / len(df_train_data)
+
+
+def normalize(src):
+    src[:, :, 0] -= mean[0]
+    src[:, :, 1] -= mean[1]
+    src[:, :, 2] -= mean[2]
+    src[:, :, 0] /= stddev[0]
+    src[:, :, 1] /= stddev[1]
+    src[:, :, 2] /= stddev[2]
+    return src
+
+
+def crop(src, x):
+    return src[x:x+input_size, x:x+input_size, :]
+
+validation_data_size = int(len(df_train_data) * 0.2)
+
+print(validation_data_size)
+
 df_valid = df_train_data[(len(df_train_data) - validation_data_size):]
 
 x_valid = []
 y_valid = []
 
+
 for f, tags in tqdm(df_valid.values, miniters=100):
-    img = cv2.resize(cv2.imread('input/train-jpg/{}.jpg'.format(f)), (input_size, input_size))
+    img = cv2.imread('input/train-jpg/{}.jpg'.format(f)).astype(np.float32)
+    img = normalize(img)
+    x = ((256 - input_size) / 2) - 1
+    img = crop(img, x)
     targets = np.zeros(17)
     for t in tags.split(' '):
         targets[label_map[t]] = 1
@@ -86,70 +125,99 @@ for f, tags in tqdm(df_valid.values, miniters=100):
     y_valid.append(targets)
 
 y_valid = np.array(y_valid, np.uint8)
-x_valid = np.array(x_valid, np.float16)
+x_valid = np.array(x_valid, np.float32)
 
-# Read and augment remaining training samples
 df_train = df_train_data[:(len(df_train_data) - validation_data_size)]
 
-x_train = []
-y_train = []
 
-for f, tags in tqdm(df_train.values, miniters=1000):
-    img = cv2.resize(cv2.imread('input/train-jpg/{}.jpg'.format(f)), (input_size, input_size))
-    targets = np.zeros(17)
-    for t in tags.split(' '):
-        targets[label_map[t]] = 1
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.flip(img, 0)  # flip vertically
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.flip(img, 1)  # flip horizontally
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.flip(img, 0)  # flip vertically
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.rotate(img, rotateCode=cv2.ROTATE_90_CLOCKWISE)
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.flip(img, 0)  # flip vertically
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.flip(img, 1)  # flip horizontally
-    x_train.append(img)
-    y_train.append(targets)
-    img = cv2.flip(img, 0)  # flip vertically
-    x_train.append(img)
-    y_train.append(targets)
+def train_generator():
 
-y_train = np.array(y_train, np.uint8)
-x_train = np.array(x_train, np.float16)
+    def transformations(src):
+        # Random crop
+        x = np.random.randint(0, 256 - input_size)
+        src = crop(src, x)
+        choice = np.random.randint(4)
+        # Random horizontal flip
+        if choice == 1:
+            src = cv2.flip(src=src, flipCode=1)
+        # Random vertical flip
+        if choice == 2:
+            src = cv2.flip(src=src, flipCode=0)
+        if choice == 3:
+            src = cv2.rotate(src=src, rotateCode=cv2.ROTATE_90_CLOCKWISE)
+            choice = np.random.randint(3)
+            # Random horizontal flip
+            if choice == 1:
+                src = cv2.flip(src=src, flipCode=1)
+            # Random vertical flip
+            if choice == 2:
+                src = cv2.flip(src=src, flipCode=0)
+        return src
 
-model.fit(x_train,
-          y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=2,
-          callbacks=callbacks,
-          validation_data=(x_valid, y_valid))
+    def shuffle_in_unison(a, b):
+        assert len(a) == len(b)
+        shuffled_a = np.empty(a.shape, dtype=a.dtype)
+        shuffled_b = np.empty(b.shape, dtype=b.dtype)
+        permutation = np.random.permutation(len(a))
+        for old_index, new_index in enumerate(permutation):
+            shuffled_a[new_index] = a[old_index]
+            shuffled_b[new_index] = b[old_index]
+        return shuffled_a, shuffled_b
+
+    while True:
+        for start in range(0, len(df_train), batch_size):
+            x_batch = []
+            y_batch = []
+            end = min(start + batch_size, len(df_train))
+            df_train_batch = df_train[start:end]
+            for f, tags in df_train_batch.values:
+                img = cv2.imread('input/train-jpg/{}.jpg'.format(f)).astype(np.float32)
+                img = normalize(img)
+                targets = np.zeros(17)
+                for t in tags.split(' '):
+                    targets[label_map[t]] = 1
+                img = transformations(img)
+                x_batch.append(img)
+                y_batch.append(targets)
+            x_batch = np.array(x_batch, np.float32)
+            y_batch = np.array(y_batch, np.uint8)
+            x_batch, y_batch = shuffle_in_unison(x_batch, y_batch)
+            yield x_batch, y_batch
+
+
+model.fit_generator(generator=train_generator(),
+                    steps_per_epoch=(len(df_train) // batch_size) + 1,
+                    epochs=epochs,
+                    verbose=2,
+                    callbacks=callbacks,
+                    validation_data=(x_valid, y_valid))
 
 df_test_data = pd.read_csv('input/sample_submission_v2.csv')
 
-x_test = []
 
-for f, tags in tqdm(df_test_data.values, miniters=1000):
-    img = cv2.resize(cv2.imread('input/test-jpg/{}.jpg'.format(f)), (input_size, input_size))
-    x_test.append(img)
+def test_generator():
+    while True:
+        for start in range(0, len(df_test_data), batch_size):
+            x_batch = []
+            end = min(start + batch_size, len(df_test_data))
+            df_test_batch = df_test_data[start:end]
+            for f, tags in df_test_batch.values:
+                img = cv2.imread('input/test-jpg/{}.jpg'.format(f)).astype(np.float32)
+                img = normalize(img)
+                x = ((256 - input_size) / 2) - 1
+                img = crop(img, x)
+                x_batch.append(img)
+            x_batch = np.array(x_batch, np.float32)
+            yield x_batch
 
-x_test = np.array(x_test, np.float16)
 
 p_valid = model.predict(x_valid, batch_size=batch_size)
 print(fbeta_score(y_valid, np.array(p_valid) > 0.2, beta=2, average='samples'))
 
 y_test = []
 
-p_test = model.predict(x_test, batch_size=batch_size, verbose=2)
+p_test = model.predict_generator(generator=test_generator(),
+                                 steps=(len(df_test_data) // batch_size) + 1)
 y_test.append(p_test)
 
 result = np.array(y_test[0])
